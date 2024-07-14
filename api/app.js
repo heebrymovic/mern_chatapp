ed6f12311011a6028aec8281058e4107cde3b322
heebrymovic <heebrymovic@gmail.com>
2024-07-14 05:04:09 +0100

    Added morgan middleware to the api

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 
 const { connectDB, app } = require('./connectDB');
 const { authRoutes, messageRoutes, userRoutes } = require('./routes');
@@ -9,6 +10,7 @@ connectDB();
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', verifyUser, messageRoutes);
