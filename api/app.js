398f8b0b044c4c6cfd10f14e428bfcb604009fbe
heebrymovic <heebrymovic@gmail.com>
2024-07-24 09:48:02 +0100

    Deployed version

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+const path = require('path');
 
 const { connectDB } = require('./connectDB');
 const { app } = require('./socket');
@@ -12,7 +13,14 @@ connectDB();
 app.use(cookieParser());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+console.log(__dirname, 'dir');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', verifyUser, messageRoutes);
 app.use('/api/user', verifyUser, userRoutes);
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'));
+});
